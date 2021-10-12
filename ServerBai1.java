d1e9576a8eda01be0939283212861af4826a821f
NgQuangLinh <91520807+NgQuangLinh@users.noreply.github.com>
2021-10-12 14:26:16 +0700

    đổi số in ra chữ

---
+++ ServerBai1.java
@@ -0,0 +1,48 @@
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+public class ServerBai1 {
+	
+	public static String Bai1(String[] so, String[]chu, String str) {
+		for(int i=0;i<extracted(so).length;i++) {
+			if(str.equalsIgnoreCase(extracted(so)[i]))
+				return extracted(chu)[i];
+		}
+		return "khong co so nguyen to nay";
+	}
+	
+	public static void main(String args[])throws Exception{
+		String so[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		String chu[] = {"khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin"};
+
+		DatagramSocket serverSocket = new DatagramSocket(9876);
+		
+		while(true) {
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[2048];
+			
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			serverSocket.receive(receivePacket);
+			
+			String sentence = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
+			System.out.print(sentence);
+			
+			InetAddress IPAddress = receivePacket.getAddress();
+			
+			int port = receivePacket.getPort();
+			
+			String sentence_to_client = sentence_to_client.Bai1(so, extracted(chu), sentence)+ " (server accpeted!)";
+			
+			sendData = sentence_to_client.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+			serverSocket.send(sendPacket); 	
+		}
+		
+	}
+
+    private static String[] extracted(String[] chu) {
+        return chu;
+    }
+}
